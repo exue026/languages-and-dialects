d147e80d251db4b93c48b3866cc6ba9332c1a0e2
Ethan Xue <ethan.xue@gmail.com>
2018-11-04 18:36:30 -0800

    organize scala code

--- scala/src/main/scala/tutorial/Main.scala
+++ scala/src/main/scala/tutorial/Main.scala
@@ -1,52 +1,63 @@
 package tutorial
 
-object Main extends App {
-  // This is a single line comment
-  /* This is a multi-line comment
-  *  The primitive data types in the language that we care about are:
-  *  Boolean
-  *  Char
-  *  Int
-  *  Double
-  * */
+object Main {
+  def main(args: Array[String]): Unit = {
+    variablesAndExpressions()
+    conditionals()
+    loops()
+  }
 
-  // Variables
+  def variablesAndExpressions(): Unit = {
+    // This is a single line comment
+    /* This is a multi-line comment
+    *  The primitive data types in the language that we care about are:
+    *  Boolean
+    *  Char
+    *  Int
+    *  Double
+    * */
 
-  /// This is a constant
-  val constantInt: Int = 3
-  println(constantInt)
+    // Variables
+    /// This is a constant
+    val constantInt: Int = 3
+    println(constantInt)
 
-  // Example of type inference
-  val constantIntInferred = 1
-  println(constantIntInferred)
+    // Example of type inference
+    val constantIntInferred = 1
+    println(constantIntInferred)
 
-  // This is a mutable value
-  var mutableInt = 0
-  mutableInt += 3
-  println(mutableInt)
+    // This is a mutable value
+    var mutableInt = 0
+    mutableInt += 3
+    println(mutableInt)
 
-  // Blocks: allows expressions to be combined together
-  // The result of the last expression is the result of the block
-  println({
-    val x = 1 + 2
-    x * 3
-  })
+    // Blocks: allows expressions to be combined together
+    // The result of the last expression is the result of the block
+    println({
+      val x = 1 + 2
+      x * 3
+    })
+  }
 
+  def conditionals(): Unit = {
+    // Conditionals
+    val age = 18
+    // Example of string literal; canVote is of type String
+    val canVote = if (age >= 18)  "yes" else "no"
+    println(canVote)
 
-  // Conditionals
-  val age = 18
-  // Example of string literal; canVote is of type String
-  val canVote = if (age >= 18)  "yes" else "no"
-  println(canVote)
+    // Generic if statement
+    if (age >= 5 && age <= 7) {
+      println("Go to kindergarten")
+    } else if (age > 6 && age <= 7) {
+      println("Got to grade 1")
+    } else {
+      println("Go to grade 2 and above")
+    }
+  }
 
-  // Generic if statement
-  if (age >= 5 && age <= 7) {
-    println("Go to kindergarten")
-  } else if (age > 6 && age <= 7) {
-    println("Got to grade 1")
-  } else {
-    println("Go to grade 2 and above")
+  def loops(): Unit = {
+
   }
 }
-
 
